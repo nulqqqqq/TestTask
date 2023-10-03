3ea939a9f4ddd99d62f7bf7326251ef96ab317a7
nulqqqqq@gmail.com <nulqqqqq@gmail.com>
2023-10-03 16:55:01 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/BuisnessLogic/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.BuisnessLogic
+{
+	public class OrderService : IOrderService
+	{
+		private readonly ApplicationDbContext db;
+
+		public OrderService(ApplicationDbContext context)
+		{
+			db = context;
+		}
+
+		public async Task<Order> GetOrder()
+		{
+			try
+			{
+				var orderWithMaxTotalPrice = await db.Orders
+											.OrderByDescending(m => m.Price * m.Quantity)
+											.FirstOrDefaultAsync();
+				return orderWithMaxTotalPrice;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
+
+		public async Task<List<Order>> GetOrders()
+		{
+			try
+			{
+				var orderWithProductQuantityAbove10 = await db.Orders
+													.Where(m => m.Quantity > 10)
+													.ToListAsync();
+				return orderWithProductQuantityAbove10;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
+	}
+}
---
+++ TestTask/Services/BuisnessLogic/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services.BuisnessLogic
+{
+	public class UserService : IUserService
+	{
+		private readonly ApplicationDbContext db;
+
+		public UserService(ApplicationDbContext context)
+		{
+			db = context;
+		}
+
+		public async Task<User> GetUser()
+		{
+			try
+			{
+				var userWithTheMostOrders = await db.Users
+											.OrderByDescending(m => m.Orders.Count)
+											.FirstOrDefaultAsync(); 
+
+				return userWithTheMostOrders;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}	
+		}
+
+		public async Task<List<User>> GetUsers()
+		{
+			try
+			{
+				var inactiveUsers = await db.Users
+									.Where(m => m.Status == UserStatus.Inactive)
+									.ToListAsync();
+				return inactiveUsers;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
+	}
+}
